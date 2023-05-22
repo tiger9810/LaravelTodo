239221a7754f8a39fa8d418136207380caff652b
tiger <biguptiger@gmail.com>
2023-05-22 11:19:00 +0800

    {{ url('css/style.css') }}を使用する

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,7 +8,7 @@ dd($todos)
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="{{ url('css/style.css') }}">
 </head>
 <body>
     <div class="container">
--- resources/views/todos/show.blade.php
+++ resources/views/todos/show.blade.php
@@ -6,10 +6,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="{{ url('css/style.css') }}">
 </head>
 <body>
     <div class="container">
+        <div class="back-link">
+            &laquo; <a href="/">back</a>
+        </div>
         <h2>{{ $todo }}</h2>
     </div>
 </body>
