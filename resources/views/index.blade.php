64ba0ea1e6610a44966cb437317ba8a93652ae0b
tiger <biguptiger@gmail.com>
2023-05-24 12:24:34 +0800

    todo用のformの作成

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,19 +4,45 @@
         Today's todo
     </x-slot>
 
-    <h2>Today's Todo</h2>
+    <h2>
+        <span>Today's Todo</span>
+    </h2>
+
+    <form method="post" action="">
+        @csrf
+        <div class="form-group">
+            <label for="">
+                Title
+                <input type="text" name="title">
+            </label>
+        </div>
+        <div class="form-group">
+            <label for="">
+                detail
+                <textarea name="detail"></textarea>
+            </label>
+        </div>
+        <div class="form-button">
+            <button>Add</button>
+        </div>
+
+    </form>
+
     <ul>
         {{-- $todosという配列について、その要素が存在する限り以下の処理を行います。
             各要素の値を$todoという変数に、そしてその要素のインデックスを$iという変数に格納します。
             もし$todosが空の場合（つまり要素が一つも存在しない場合）は、@empty以下の処理を行います。」 --}}
         @forelse ($todos as $todo)
-            <li>
+        <ul>
+            <li class="list-item">
                 <input type="checkbox" name="" id="">
                 {{-- <a href="/todos/{{ $index }}"> --}}
-                <a href="{{ route('todos.show', $todo->id) }}">
+                <a href="{{ route('todos.show', $todo) }}">
                     {{ $todo->title }}
                 </a>
             </li>
+        </ul>
+
         @empty
             <li>Todo Nothing!!</li>
         @endforelse
